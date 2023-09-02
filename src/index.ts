f784be2604a42ae91629859be609f39d26645275
enesflow <enes@kavcakar.com.tr>
2023-09-02 22:35:48 +0300

    Fixed body is empty

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,27 @@
 // open-prisma-data-proxy
-// Open source Prisma data proxy
-// For the first step, we need to find out what the request is
 import express from "express";
-import bodyParser from "body-parser";
+import https from "https";
+import fs from "fs";
+
+const key = fs.readFileSync("./certs/selfsigned.key");
+const cert = fs.readFileSync("./certs/selfsigned.crt");
+const options = {
+	key,
+	cert
+}
+
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 const PORT = 3000;
 
 app.all("/:version/:id/graphql", (req, res) => {
 	console.log(req.body) // {}
 	console.log(req.params)
 	console.log(req.headers)
-	// I think the reason req.body is empty is the "connection" type is "keep-alive"
-	// It might be because I'm using Cloudflare Tunnels to test this
-	// Because otherwise I get SSL errors on http://localhost:3000
-	res.send("DEBUG")
+	res.send("{data: {hello: 'world'}}")
 })
 
-app.listen(PORT, () => {
-	console.log(`🔅 Server is listening on port ${PORT}`);
-});
+https.createServer(options, app).listen(PORT, () => {
+		console.log(`🔅 Server is listening on port ${PORT}`);
+	}
+);
