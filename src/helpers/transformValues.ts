ec76a4a1a6b7aa5a6b841d58d0acb536327e81f2
enesflow <enes@kavcakar.com.tr>
2023-09-03 17:57:00 +0300

    Fixed transformField

--- src/helpers/transformValues.ts
+++ src/helpers/transformValues.ts
@@ -1,49 +1,22 @@
-import {DMMF} from "@prisma/client/runtime/library";
-import {getDMMF} from "@prisma/internals";
-import fs from "fs";
-import {RequestBody} from "../index.js";
-
-const dmmf = await getDMMF({
-	datamodel: fs.readFileSync("./prisma/schema.prisma", "utf-8"),
-})
-
-function getModel(modelName: string) {
-	return dmmf.datamodel.models.find(model => model.name === modelName);
-}
-
-function transformField(field: DMMF.Field, value: any) {
-	if (field.type === "DateTime") {
+function transformField(value: any) {
+	if (value.$type === "DateTime") {
 		return new Date(value.value);
 	}
 	return value;
 }
 
 
-export function transformValues(body: RequestBody) {
-	const model = getModel(body.modelName);
-	if (!model) {
-		throw new Error(`Model ${body.modelName} not found`);
-	}
-	const data = body.query.arguments.data;
-	for (const field of model.fields) {
-		if (!data[field.name]) {
-			continue;
-		}
-		if (field.relationName) {
-			data[field.name] = transformValues({
-				modelName: field.type,
-				action: "create",
-				query: {
-					arguments: {
-						data: data[field.name],
-					}
-				}
-			})
-		} else {
-			data[field.name] = transformField(field, data[field.name]);
+export function transformValues(data: any) {
+	for (const key in data) {
+		if (typeof data[key] === "object") {
+			if (data[key].$type) {
+				data[key] = transformField(data[key]);
+			} else {
+				data[key] = transformValues(data[key]);
+			}
 		}
 	}
-	return body;
+	return data;
 }
 
 export function transformSelection(selection: any) {
--- src/index.ts
+++ src/index.ts
@@ -51,45 +51,55 @@ app.use(express.json());
 
 app.use((req, res, next) => {
 	if (!req.headers.authorization) {
+		console.log("No authorization header")
 		res.status(401).send("Unauthorized")
+		return;
 	} else {
 		const auth = req.headers.authorization.trim();
 		if (auth !== `Bearer ${TOKEN}`) {
+			console.log("Invalid authorization header", auth)
 			res.status(401).send("Unauthorized")
+			return;
 		}
 		next();
 	}
 })
 
 app.all("/:version/:id/graphql", async (req, res) => {
-	try {
+		try {
 
-		let body = req.body as RequestBody;
-		body = transformAction(body);
-		if (body.action.startsWith("create")) {
-			body = transformValues(body);
-		}
-		body.query.selection = transformSelection(body.query.selection);
-		body.query.arguments.data = transformData(body.query.arguments.data)
-		console.dir(body, {depth: null, colors: true}); // <- For debugging
-		const result = await (prisma as any)[body.modelName][body.action](
-			convertToPrismaQuery(body)
-		)
+			let body = req.body as RequestBody;
+			console.log("Before:")
+			console.dir(body, {depth: null, colors: true}); // <- For debugging}
+			body = transformAction(body);
+			body.query.arguments = transformValues(body.query.arguments);
+			body.query.selection = transformSelection(body.query.selection);
+			body.query.arguments.data = transformData(body.query.arguments.data)
+			console.log("After:")
+			console.dir(body, {depth: null, colors: true}); // <- For debugging
+			const result = await (prisma as any)[body.modelName][body.action](
+				convertToPrismaQuery(body)
+			)
 
 
-		const sessionName = body.action + "Session";
-		const data = {
-			data: {
-				[sessionName]: result
+			const sessionName = body.action + "Session";
+			const data = {
+				data: {
+					[sessionName]: result
+				}
 			}
+			console.log("Result:")
+			console.dir(data, {depth: null, colors: true}); // <- For debugging
+			res.send(JSON.stringify(data));
+		} catch
+			(e) {
+			console.error("Failed to process request");
+			console.error(e);
+
+			res.status(500).send(e);
 		}
-		res.send(JSON.stringify(data));
-	} catch (e) {
-		console.error("Failed to process request");
-		console.error(e);
-		res.status(500).send(e);
 	}
-})
+)
 
 const startMessage = `🔅 Server is listening on port ${PORT}`;
 if (process.env.SELF_SIGNED_CERT) {
